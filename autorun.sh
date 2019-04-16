8abbf064b2c8ebec22ed70bd679029cd2f615102
Tanmoy Das <tanmoydas1997@gmail.com>
2019-04-17 03:31:29 +0530

    Added loop to autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 #echo "Activating conda environment"
 #source activate base
-echo "$PWD" | python ../../../dlib_land.py
+
+   
+cd "subject1"
+
+for s in {anger,disgust,fear,happiness,sadness,surprise}
+do
+    cd $s
+    echo "Entering $s"
+    echo "$PWD" | python ../../dlib_land.py
+    
+    echo | python ../../displacement_vector.py
+    echo | python ../../landmark_single.py
+
+    cd ..
+    echo "Exited $s"
+done
+cd ..
+
+#echo "$PWD" | python ../../../dlib_land.py
 
 
 
